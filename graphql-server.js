efe369869013c04e9a25ee83aae7da16d8d214a0
John Resig <jeresig@gmail.com>
2017-12-03 08:00:46 -0500

    Implement GraphQL and REST API servers backed by MongoDB/Mongoose models.

---
+++ graphql-server.js
@@ -0,0 +1,59 @@
+"use strict";
+const graphqlHTTP = require("express-graphql");
+const {buildSchema} = require("graphql");
+const express = require("express");
+const app = express();
+
+// Get the Mongoose models used for querying the database
+const {User, Group} = require("./models.js");
+
+// Start up a GraphQL endpoint listening at /graphql
+app.use("/graphql", graphqlHTTP({
+    // We construct our GraphQL schema which has three types:
+    // The User, Group, and Query types (through which all
+    // queries for data are defined)
+    schema: buildSchema(`
+        type Group {
+            _id: String
+            name: String
+        }
+
+        type User {
+            _id: String
+            username: String
+            group: Group
+        }
+
+        type Query {
+            user(id: String!): User
+            users: [User]
+            group(id: String!): Group
+            groups: [Group]
+        }
+    `),
+    // The methods that we'll use to get the data for our
+    // main queries
+    rootValue: {
+        // Get a user based on the ID and return it as a Promise
+        user({id}) {
+            return User.findById(id);
+        },
+        // Get an array of users and return them as a Promise
+        users() {
+            return User.find({});
+        },
+        // Get a group based on the ID and return it as a Promise
+        group({id}) {
+            return Group.findById(id);
+        },
+        // Get an array of groups and return them as a Promise
+        groups() {
+            return Group.find({});
+        },
+    },
+    // Display the GraphiQL web interface (for easy usage!)
+    graphiql: true,
+}));
+
+// Start the application, listening on port 3000
+app.listen(3000);
---
+++ models.js
@@ -0,0 +1,36 @@
+"use strict";
+const mongoose = require("mongoose");
+
+// Connect to the local MongoDB database named “testdb”
+mongoose.connect("mongodb://localhost/testdb",
+    {useMongoClient: true});
+
+// Use real promises
+mongoose.Promise = global.Promise;
+
+// Create a Group schema to be stored in the MongoDB database
+const GroupSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+});
+
+// Turn that schema into a model that we can query
+const Group = mongoose.model("Group", GroupSchema);
+
+// Create a User schema to be stored in the MongoDB database
+const UserSchema = new mongoose.Schema({
+    _id: String,
+    username: String,
+    groupId: String,
+});
+
+// Retrieve the group associated with the user
+UserSchema.methods.group = function() {
+    // Use .exec() to ensure a true Promise is returned
+    return Group.findById(this.groupId).exec();
+};
+
+// Turn that schema into a model that we can query
+const User = mongoose.model("User", UserSchema);
+
+module.exports = {User, Group};
