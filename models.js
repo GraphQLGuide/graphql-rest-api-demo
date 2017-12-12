d4580748e051118804ecbfd16f1c3317fd94e51b
Loren Sands-Ramshaw <lorensr@gmail.com>
2017-12-11 22:03:39 -0500

    Prettify

--- models.js
+++ models.js
@@ -1,36 +1,35 @@
-"use strict";
-const mongoose = require("mongoose");
+'use strict'
+const mongoose = require('mongoose')
 
 // Connect to the local MongoDB database named “testdb”
-mongoose.connect("mongodb://localhost/testdb",
-    {useMongoClient: true});
+mongoose.connect('mongodb://localhost/testdb', { useMongoClient: true })
 
 // Use real promises
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 // Create a Group schema to be stored in the MongoDB database
 const GroupSchema = new mongoose.Schema({
-    _id: String,
-    name: String,
-});
+  _id: String,
+  name: String
+})
 
 // Turn that schema into a model that we can query
-const Group = mongoose.model("Group", GroupSchema);
+const Group = mongoose.model('Group', GroupSchema)
 
 // Create a User schema to be stored in the MongoDB database
 const UserSchema = new mongoose.Schema({
-    _id: String,
-    username: String,
-    groupId: String,
-});
+  _id: String,
+  username: String,
+  groupId: String
+})
 
 // Retrieve the group associated with the user
 UserSchema.methods.group = function() {
-    // Use .exec() to ensure a true Promise is returned
-    return Group.findById(this.groupId).exec();
-};
+  // Use .exec() to ensure a true Promise is returned
+  return Group.findById(this.groupId).exec()
+}
 
 // Turn that schema into a model that we can query
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema)
 
-module.exports = {User, Group};
+module.exports = { User, Group }
--- rest-server.js
+++ rest-server.js
@@ -1,75 +1,76 @@
-"use strict";
-const express = require("express");
-const app = express();
+'use strict'
+const express = require('express')
+const app = express()
 
 // Get the Mongoose models used for querying the database
-const {User} = require("./models.js");
+const { User } = require('./models.js')
 
 // A list of the fields that are allowed to be accessed
-const defaultFields = ["_id", "username", "group"];
+const defaultFields = ['_id', 'username', 'group']
 
 // Filter a user object based on the requested fields
 const filterFields = async function(req, user) {
-    // We assume the fields are a comma-separated list of field
-    // names, if none is specified then we return all fields.
-    const fieldKeys = req.query.fields
-        ? req.query.fields.split(",")
-        : defaultFields;
+  // We assume the fields are a comma-separated list of field
+  // names, if none is specified then we return all fields.
+  const fieldKeys = req.query.fields
+    ? req.query.fields.split(',')
+    : defaultFields
 
-    // Generate a new object that contains only those fields.
-    const filteredUser = {};
-    for (const field of fieldKeys) {
-        // If the field is a function then we expect it to return
-        // a promise which we will immediately resolve.
-        if (typeof user[field] === "function") {
-            filteredUser[field] = await user[field]();
-        } else {
-            filteredUser[field] = user[field];
-        }
+  // Generate a new object that contains only those fields.
+  const filteredUser = {}
+  for (const field of fieldKeys) {
+    // If the field is a function then we expect it to return
+    // a promise which we will immediately resolve.
+    if (typeof user[field] === 'function') {
+      filteredUser[field] = await user[field]()
+    } else {
+      filteredUser[field] = user[field]
     }
-    return filteredUser;
-};
+  }
+  return filteredUser
+}
 
 // Listen for all GET requests to /users/:id URL (where the
 // ID is the ID of the user account)
-app.get("/users/:id", (req, res) => {
-    // Try to find the user by their id (_id field), using the ID
-    // parameter from the URL.
-    User.findById(req.params.id, async (err, user) => {
-        if (err) {
-            // The DB returned an error so we return a 500 error
-            return res.status(500).end();
-        }
+app.get('/users/:id', (req, res) => {
+  // Try to find the user by their id (_id field), using the ID
+  // parameter from the URL.
+  User.findById(req.params.id, async (err, user) => {
+    if (err) {
+      // The DB returned an error so we return a 500 error
+      return res.status(500).end()
+    }
 
-        if (!user) {
-            // No user was found so we return a 404 error
-            return res.status(404).end();
-        }
+    if (!user) {
+      // No user was found so we return a 404 error
+      return res.status(404).end()
+    }
 
-        // Return the user to the client (automatically serialized
-        // as a JSON string). We need to wait for all of the fields
-        // to load before we can return the results.
-        res.send(await filterFields(req, user));
-    });
-});
+    // Return the user to the client (automatically serialized
+    // as a JSON string). We need to wait for all of the fields
+    // to load before we can return the results.
+    res.send(await filterFields(req, user))
+  })
+})
 
 // Listen for all GET requests to /users
-app.get("/users", (req, res) => {
-    // Find all of the users in the database collection (we pass in
-    // an empty collection as we aren't filtering the results)
-    User.find({}, async (err, users) => {
-        if (err) {
-            // The DB returned an error so we return a 500 error
-            return res.status(500).end();
-        }
+app.get('/users', (req, res) => {
+  // Find all of the users in the database collection (we pass in
+  // an empty collection as we aren't filtering the results)
+  User.find({}, async (err, users) => {
+    if (err) {
+      // The DB returned an error so we return a 500 error
+      return res.status(500).end()
+    }
 
-        // Return the array of users to the client (automatically
-        // serialized as a JSON string) We need to wait for all
-        // of the Promises to resolve for all of the users.
-        res.send(await Promise.all(users.map(async (user) =>
-            await filterFields(req, user))));
-    });
-});
+    // Return the array of users to the client (automatically
+    // serialized as a JSON string) We need to wait for all
+    // of the Promises to resolve for all of the users.
+    res.send(
+      await Promise.all(users.map(async user => await filterFields(req, user)))
+    )
+  })
+})
 
 // Start the application, listening on port 3000
-app.listen(3000);
+app.listen(3000)
